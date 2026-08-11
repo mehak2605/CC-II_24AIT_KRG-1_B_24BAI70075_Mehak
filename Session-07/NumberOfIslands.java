25953845386ba9efd586b057fa18d730f7e744f7
mehak2605 <70470410+mehak2605@users.noreply.github.com>
2026-08-11 11:01:10 +0530

    Uploaded session 7

---
+++ Session-07/NumberOfIslands.java
@@ -0,0 +1,31 @@
+class Solution {
+    public void solve(char grid[][], int i, int j)
+    {
+        if(i>=grid.length || i<0 || j>=grid[0].length || j<0)
+        return;
+
+        if(grid[i][j]=='1')
+        {
+            grid[i][j]='2';
+            solve(grid, i+1, j);
+            solve(grid, i-1, j);
+            solve(grid, i, j+1);
+            solve(grid, i, j-1);
+        }
+    }
+    public int numIslands(char[][] grid) {
+        int count=0;
+        for(int i=0; i<grid.length; i++)
+        {
+            for(int j=0; j<grid[0].length; j++)
+            {
+                if(grid[i][j]=='1')
+                {
+                    solve(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
